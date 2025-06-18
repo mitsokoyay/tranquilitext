6741e309846f7f10b643866bf94859490c8b82f4
Dylan Berutti <beruttid@gmail.com>
2025-06-18 15:49:00 -0400

    second commit, working with ratatui. starting to understand the gist of this.

--- main.rs
+++ main.rs
@@ -1,6 +1,20 @@
-use std::process::Command;
+use crossterm::event::{self, Event};
+use ratatui::{text::Text, Frame};
 
-fn main() {
-    println!("running vim: ");
-    let input = Command::new("vim").output().expect("failed to execute");
+
+fn main(){
+    let mut terminal = ratatui::init();
+    loop{
+        terminal.draw(draw).expect("failed to draw frame");
+        if matches!(event::read().expect("failed to read"), Event::Key(_)){
+            break;
+        }
+    }
+    ratatui::restore();
+}
+
+
+fn draw(frame: &mut Frame){
+    let text = Text::raw("learning ratatui.... seems interesting so far!!");
+    frame.render_widget(text, frame.area());
 }
