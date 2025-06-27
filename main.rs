8483ba251d906617a6c6ab08937b9f71b6de873d
Dylan Berutti <beruttid@gmail.com>
2025-06-27 14:42:16 -0400

    This is commited

--- main.rs
+++ main.rs
@@ -1,20 +1,30 @@
 use crossterm::event::{self, Event};
-use ratatui::{text::Text, Frame};
+use ratatui::{Frame, text::Text};
+use std::{thread, time::Duration};
 
-
-fn main(){
+fn main() {
     let mut terminal = ratatui::init();
-    loop{
+    loop {
         terminal.draw(draw).expect("failed to draw frame");
-        if matches!(event::read().expect("failed to read"), Event::Key(_)){
+        if matches!(event::read().expect("failed to read"), Event::Key(_)) {
             break;
         }
+        thread::sleep(Duration::from_secs(3));
+        terminal.draw(draw2).expect("failed to draw frame");
+        if matches!(event::read().expect("failed to read"), Event::Key(_)) {
+            break;
+        }
+        thread::sleep(Duration::from_secs(3));
     }
     ratatui::restore();
 }
 
-
-fn draw(frame: &mut Frame){
+fn draw(frame: &mut Frame) {
     let text = Text::raw("learning ratatui.... seems interesting so far!!");
+    frame.render_widget(text, frame.area());
+}
+
+fn draw2(frame: &mut Frame) {
+    let text = Text::raw("heres another thing im drawing");
     frame.render_widget(text, frame.area());
 }
